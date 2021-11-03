1171f36c655cc9e7b95b004250c0dec417d810f9
Dknife <young.min.kang@gmail.com>
2021-11-03 09:47:54 +0900

    Add files via upload

---
+++ Source/07_3DScene/3DObjects_02Rects.py
@@ -0,0 +1,72 @@
+
+from PyQt5.QtWidgets import *
+
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import math
+import sys
+
+class MyOpenGLWidget(QOpenGLWidget):
+    def __init__(self):
+        super().__init__()
+
+    def initializeGL(self):
+        glClearColor(0.5, 0.5, 0.5, 1.0)        
+
+    def paintGL(self):
+        glClear(GL_COLOR_BUFFER_BIT)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        glTranslate(0.5, 0, 0)
+        draw_axes()
+        
+        glColor3f(1, 1, 0)
+        glBegin(GL_LINE_LOOP)
+        glVertex3f(-0.5, -0.5, 1)
+        glVertex3f( 0.5, -0.5, 1)
+        glVertex3f( 0.5,  0.5, 1)
+        glVertex3f(-0.5,  0.5, 1)
+        glEnd()
+
+        glColor3f(0, 1, 1)
+        glBegin(GL_LINE_LOOP)
+        glVertex3f(-0.5, -0.5, 0)
+        glVertex3f( 0.5, -0.5, 0)
+        glVertex3f( 0.5,  0.5, 0)
+        glVertex3f(-0.5,  0.5, 0)
+        glEnd()
+        
+
+    def resizeGL(self, w, h):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(-2, 2, -2, 2, -2, 2)
+        
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    win = MyOpenGLWidget()
+    win.show()
+
+    app.exec()
+
+def draw_axes():
+    #### 축 그리기
+    glBegin(GL_LINES)
+    glColor3f(1,0,0)
+    glVertex3f(0,0,0)
+    glVertex3f(1,0,0)
+    glColor3f(0,1,0)
+    glVertex3f(0,0,0)
+    glVertex3f(0,1,0)
+    glColor3f(0,0,1)
+    glVertex3f(0,0,0)
+    glVertex3f(0,0,1)
+    glEnd()
+
+###################################
+if __name__ == '__main__':
+    main()
