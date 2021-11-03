75b6ef23fcbe1d62ba2541b4a239e7086ccc6238
Dknife <young.min.kang@gmail.com>
2021-11-03 09:21:00 +0900

    Update 3DObjects.py

--- Source/07_3DScene/3DObjects.py
+++ Source/07_3DScene/3DObjects.py
@@ -1,1 +1,34 @@
-t
+
+from PyQt5.QtWidgets import *
+
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import math
+import sys
+
+class MyOpenGLWidget(QOpenGLWidget):
+    def __init__(self):
+        super().__init__()
+
+    def initializeGL(self):
+        glClearColor(0.5, 0.5, 0.5, 1.0)        
+
+    def paintGL(self):
+        glClear(GL_COLOR_BUFFER_BIT)
+
+    def resizeGL(self, w, h):
+        pass
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    win = MyOpenGLWidget()
+    win.show()
+
+    app.exec()
+
+###################################
+if __name__ == '__main__':
+    main()
